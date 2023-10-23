76ff299eb5753e73830539569423ef2fd3b27c73
demisu-s <damisusefu@gmail.com>
2023-10-23 16:42:05 +0300

    adding cors

--- api/server.js
+++ api/server.js
@@ -1,42 +1,37 @@
 const { config } = require('dotenv');
 const express = require('express');
 const app = express();
-const dotenv=require('dotenv')
-const morgan=require('morgan')
-const bodyParser=require('body-parser')
-const path=require('path')
-const connectDB=require('./server/database/connection')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const connectDB = require('./server/database/connection');
+const cors = require('cors');
 
-dotenv.config({path:'config.env'})
-const PORT=process.env.PORT || 8080
-//log request
-app.use(morgan('tiny'))
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+// log request
+app.use(morgan('tiny'));
 
-//mongoDB connection 
-
+// MongoDB connection
 connectDB();
 
-//parse request to body-parser
-app.use(bodyParser.urlencoded({extended:true}))
- 
-//set view engine
-app.set("view engine","ejs")
-// app.set("views",path.resolve(__dirname,""))
+// Parse request to body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set view engine
+app.set("view engine", "ejs");
 
+// Load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/image', express.static(path.resolve(__dirname, "assets/image")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
-//load assets
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/image',express.static(path.resolve(__dirname,"assets/image")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
-//if i want to access to style.css file 
-//the only thing i have to do is just write in this way
-//css/style.css
+// Enable CORS
+app.use(cors());
 
-//load route to server app
-
-app.use('/',require('./server/routes/router'))
-
+// Load routes to serve app
+app.use('/', require('./server/routes/router'));
 
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
